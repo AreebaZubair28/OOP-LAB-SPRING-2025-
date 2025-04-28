d8a69aef825063bf96792cf8cb46662086caebbc
AreebaZubair28 <areebazubair28@gmail.com>
2025-04-29 00:37:27 +0500

    Create Task09.cpp

---
+++ Lab 10/Task09.cpp
@@ -0,0 +1,28 @@
+#include "iostream"
+#include "fstream"
+using namespace std;
+
+int main() {
+    ifstream inFile;
+    inFile.open("large_log.txt", ios::binary);
+    if(!inFile) {
+        cerr << "File can't be opened!\n";
+        exit(1);
+    }
+
+    char buffer[100] = {0};
+    inFile.seekg(ios :: beg);
+    inFile.read(buffer, 10);
+    streampos pos = inFile.tellg();
+    cout << "Current position is: " << pos << endl;
+
+    inFile.read(buffer, 10);
+    pos = inFile.tellg();
+    cout << "Current position is: " << pos << endl;
+
+    inFile.read(buffer, 10);
+    pos = inFile.tellg();
+    cout << "Current position is: " << pos << endl;
+
+    inFile.close();
+}
